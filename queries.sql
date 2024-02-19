3ad2c599dc88933e1dd0d70d9debab028ed29898
briannaclarkso <brianna.extra@gmail.com>
2024-02-18 21:45:48 -0600

    Im passing tests but it's not working

--- queries.sql
+++ queries.sql
@@ -2,8 +2,25 @@
 
 --Part 2
 
-SELECT name FROM employer WHERE location = "St. Louis City";
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 --Part 3
 
+DROP TABLE job;
+
 --Part 4
+
+--SELECT * FROM skill
+--FROM skill
+--LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+--WHERE job_skills.job_id IS NOT NULL
+--ORDER BY name ASC;
+
+SELECT *
+FROM skill
+LEFT JOIN job_skills
+ON job_skills.skills_id = skill.id
+ORDER BY name ASC;
+
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,10 +1,17 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs =  new ArrayList<Job>();
 
     @Size(min =5, max = 200)
     public String description;
@@ -17,5 +24,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
